451eb9f62683975933125bdccc9d872d88269bd7
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-11-01 18:00:00 +0800

    fix: update wrapper

--- epan-sys/wrapper.h
+++ epan-sys/wrapper.h
@@ -1,13 +1,12 @@
 #ifndef __EPAN_SYS
 #define __EPAN_SYS
 
-#include <wiretap/wtap.h>
-// #include "epan/packet.h"
-#include "epan/decode_as.h"
 #include <wireshark.h>
+#include <wiretap/wtap.h>
 #include <wsutil/plugins.h>
 #include <epan/packet.h>
 #include <epan/proto.h>
+#include <epan/decode_as.h>
 
 
 
