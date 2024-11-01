febd3b2615183481b0a5b010644f63975bdf27ba
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-11-01 18:19:31 +0800

    fix: paths for helper script on macOS

--- build-examples.sh
+++ build-examples.sh
@@ -2,13 +2,37 @@
 
 #
 # Helper script to build all our example dissectors, and copies them to where Wireshark will search
+# Currently this 
 #
+
 cargo build --examples
-epan_dir=~/.local/lib/wireshark/plugins/4.4/epan/
-for file in wsdf/examples/*
+
+case "$OSTYPE" in
+  darwin*) 
+    epan_dir="/Applications/Wireshark.app/Contents/PlugIns/wireshark/4-4/epan"
+    lib_ext="dylib"
+    ;;
+  linux*)  
+    epan_dir="$HOME/.local/lib/wireshark/plugins/4.4/epan"
+    lib_ext="so";;
+	*)
+    echo "Unsupported OS: $OSTYPE for this script."
+    exit 1
+    ;;
+esac
+
+mkdir -p "$epan_dir"
+
+for file in wsdf/examples/*;
 do
-	filename=$(basename -- "$file")
-	example="${filename%.*}"
-	shared_obj="target/debug/examples/lib${example}.so"
-	cp "$shared_obj" "$epan_dir"
+    filename=$(basename -- "$file")
+    example="${filename%.*}"
+    shared_obj="target/debug/examples/lib${example}.${lib_ext}"
+
+    if [ -f "$shared_obj" ]; then
+        cp "$shared_obj" "${epan_dir}/lib${example}.so"
+        echo "Copied ${shared_obj} to ${epan_dir}/lib${example}.so"
+    else
+        echo "Warning: ${shared_obj} not found, skipping."
+    fi
 done
