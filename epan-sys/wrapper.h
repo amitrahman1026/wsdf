7e98d6e30c6fbe97fe65608c2e16e999f7c7ef7b
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-11-01 17:42:23 +0800

    fix: some mising headers for 4.4.x plugins

--- epan-sys/wrapper.h
+++ epan-sys/wrapper.h
@@ -1,9 +1,14 @@
 #ifndef __EPAN_SYS
 #define __EPAN_SYS
 
-#include "wiretap/wtap.h"
-#include "epan/packet.h"
+#include <wiretap/wtap.h>
+// #include "epan/packet.h"
 #include "epan/decode_as.h"
+#include <wireshark.h>
+#include <wsutil/plugins.h>
+#include <epan/packet.h>
+#include <epan/proto.h>
+
 
 
 #endif // __EPAN_SYS
